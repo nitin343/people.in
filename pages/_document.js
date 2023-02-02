9a7bcb733ec65557064a4c7cb7b1793b3dcb1a79
Niteen <nitinphulekar33@gmail.com>
2023-02-02 10:57:40 +0530

    changed desgin improvement filters

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+      {/* <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta> */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
