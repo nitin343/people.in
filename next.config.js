0581ed709692e3db6e51d0d42add5f7a582d7def
nitin343 <61547659+nitin343@users.noreply.github.com>
2023-03-22 20:11:21 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-const ContentSecurityPolicy = `
-default-src 'self';
-script-src 'self';
-child-src example.com;
-style-src 'self' example.com;
-font-src 'self';  
-`
-
 module.exports = {
   async headers() {
     return [
-      {
-        key: 'Content-Security-Policy',
-        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-      }
+      { 
+        source: '/:path*{/}?',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value for all pages',
+          },
+        ],
+      },
     ]
   },
 }
-
