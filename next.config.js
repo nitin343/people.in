d8eb1c44884374886ef42bf7a5476257ac15c80f
nitin343 <61547659+nitin343@users.noreply.github.com>
2023-03-22 20:00:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,23 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const ContentSecurityPolicy = `
+default-src 'self';
+script-src 'self';
+child-src example.com;
+style-src 'self' example.com;
+font-src 'self';  
+`
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+      }
+    ]
+  },
+}
+
